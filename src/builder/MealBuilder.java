915d0093e4b4f166cab58a8cdb96d602d09945af
Ali Ashraf <mmaashraf798@gmail.com>
2025-04-13 18:30:20 -0700

    Initial: Purpose is to capture the learning,notes, practice code while i read up on Design Patterns. Learned the 5 creational patterns, and now practicing implementation. Todo: complete the implementation, add my problem statements in a readme, and also the notes i have made so far if needed

---
+++ src/builder/MealBuilder.java
@@ -0,0 +1,21 @@
+/*
+
+this will be builder, that is responsible for assembling ingredients and returning a product, 
+that is of type Meal
+It should defines the common methods that are needed to construct the product
+-- 
+
+the concrete implementation of this interface should take of instance creating, reset, and return
+*/
+
+package builder;
+
+import factory.Meal;
+
+public interface MealBuilder {
+    Meal getProduct();
+    void addIngreds(String name);
+    void removeIngreds(String name);
+    int getTotalCost();
+    void reset();
+}
---
+++ src/factory/Meal.java
@@ -0,0 +1,9 @@
+// this will be the product interface that factory methods produce
+
+package factory;
+
+public interface Meal {
+    void addIngrd(String name);
+    void removeIngrd(String name);
+    int getTotalCost();
+}
